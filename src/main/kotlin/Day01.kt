e1a08cf82fa2fdfd18f897aa865a77759dab42fd
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2020-12-01 10:27:28 +0100

    Solve day1 part 2

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,15 +1,28 @@
 class Day01 {
     fun solvePart1(input: String): Int? {
         val numbers = parse(input)
-        return productOfPairThatSumsTo2020(numbers)
+        return productOfPairThatSumsTo(2020, numbers.sorted())
     }
 
-    private fun productOfPairThatSumsTo2020(numbers: List<Int>): Int? {
-        val sortedNumbers = numbers.sorted()
-        val target = 2020
+    fun solvePart2(input: String): Int? {
+        val numbers = parse(input)
+        return productOfTripleThatSumsTo2020(numbers.sorted())
+    }
 
-        var left = 0
-        var right = numbers.lastIndex
+    private fun productOfTripleThatSumsTo2020(sortedNumbers: List<Int>): Int? {
+        for ((index, number) in sortedNumbers.withIndex()) {
+            if(number > 2020) break
+            val remainingTo2020 = 2020 - number
+
+            productOfPairThatSumsTo(remainingTo2020, sortedNumbers, index + 1)
+                ?.let { return it * number }
+        }
+        return null
+    }
+
+    private fun productOfPairThatSumsTo(target: Int, sortedNumbers: List<Int>, startIndex: Int = 0): Int? {
+        var left = startIndex
+        var right = sortedNumbers.lastIndex
 
         while (left < right) {
             val currentSum = sortedNumbers[left] + sortedNumbers[right]
--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -18,8 +18,29 @@ class Day01Test {
 
     @Test
     fun `part1 actual data`() {
-        val input = Resources.read("01-1.txt")
+        val input = Resources.read("01.txt")
 
         assertEquals(840324, Day01().solvePart1(input))
+    }
+
+    @Test
+    fun `part2 provided example`() {
+        val input = """
+            1721
+            979
+            366
+            299
+            675
+            1456
+        """.trimIndent()
+
+        assertEquals(241861950, Day01().solvePart2(input))
+    }
+
+    @Test
+    fun `part2 actual data`() {
+        val input = Resources.read("01.txt")
+
+        assertEquals(170098110, Day01().solvePart2(input))
     }
 }
