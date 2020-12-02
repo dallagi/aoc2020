74d56e80f411dba6271ffc082968a1b839b4b405
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2020-12-02 15:29:38 +0100

    Minor refactoring

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -1,11 +1,11 @@
 // Solves Day 2: Password Philosophy https://adventofcode.com/2020/day/2
 class Day02 {
     fun solvePart1(input: String): Int {
-        return entriesFrom(input).count(::correctByFrequency)
+        return entriesFrom(input).count { it.correctByFrequency() }
     }
 
     fun solvePart2(input: String): Int {
-        return entriesFrom(input).count(::correctByPosition)
+        return entriesFrom(input).count { it.correctByPosition() }
     }
 
     private fun entriesFrom(input: String) = input.split("\n").map(::parseEntry)
@@ -25,20 +25,16 @@ class Day02 {
     }
 }
 
-private data class PasswordEntry(
-    val policyNumbers: Pair<Int, Int>,
-    val policyLetter: Char,
-    val password: String
-)
+private class PasswordEntry(
+    private val policyNumbers: Pair<Int, Int>,
+    private val policyLetter: Char,
+    private val password: String
+) {
+    fun correctByFrequency() = password.count { it == policyLetter } in policyNumbers.toRange()
 
-private fun correctByFrequency(passwordEntry: PasswordEntry) = with(passwordEntry) {
-    val allowedFrequency = IntRange(policyNumbers.first, policyNumbers.second)
+    fun correctByPosition() = matchesAt(policyNumbers.first) xor matchesAt(policyNumbers.second)
 
-    password.count { it == policyLetter } in allowedFrequency
+    private fun matchesAt(index: Int) = password.getOrNull(index - 1) == policyLetter
 }
 
-private fun correctByPosition(passwordEntry: PasswordEntry) = with(passwordEntry) {
-    val matchesAt = { index: Int -> password.getOrNull(index-1) == policyLetter }
-
-    matchesAt(policyNumbers.first) xor matchesAt(policyNumbers.second)
-}
+private fun Pair<Int, Int>.toRange() = IntRange(first, second)
