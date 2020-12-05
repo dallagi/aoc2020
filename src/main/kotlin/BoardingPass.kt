ac42c6f50a5964dc84d8220ebf49e990c0c90070
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2020-12-05 11:40:46 +0100

    Replace parsing logic with much simpler solution

--- src/main/kotlin/BoardingPass.kt
+++ src/main/kotlin/BoardingPass.kt
@@ -1,36 +1,19 @@
-import java.lang.Exception
-import kotlin.math.pow
-
 data class BoardingPass(val row: Int, val column: Int) {
     fun seatId() = row * 8 + column
 
     companion object {
         fun parse(input: String): BoardingPass {
             return BoardingPass(
-                binarySearch(input.slice(0..6), 'B', 'F'),
-                binarySearch(input.slice(7..9), 'R', 'L')
+                decodeNumber(input.slice(0..6), 'F', 'B'),
+                decodeNumber(input.slice(7..9), 'L', 'R')
             )
         }
 
-        private fun binarySearch(input: String, upChar: Char, downChar: Char): Int {
-            var left = 0
-            var right = (2 pow input.length) - 1
-
-            for ((index, char) in input.withIndex()) {
-                val sliceSize = 2 pow (input.length - (index + 1))
-
-                when (char) {
-                    upChar -> left += sliceSize
-                    downChar -> right -= sliceSize
-                    else -> throw Exception("Unsupported char")
-                }
-            }
-
-            if (left != right) throw Exception("Something went wrong")
-
-            return left
+        private fun decodeNumber(encoded: String, zeroChar: Char, oneChar: Char): Int {
+            return encoded
+                .replace(zeroChar, '0')
+                .replace(oneChar, '1')
+                .toInt(radix = 2)
         }
     }
 }
-
-infix fun Int.pow(exponent: Int): Int = toDouble().pow(exponent).toInt()
