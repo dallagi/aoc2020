b6dd7ce484b16addbd399bd50fa5bff94af12856
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2020-12-06 12:21:52 +0100

    Solve day6 part2

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -2,13 +2,26 @@ class Day06 {
     fun solvePart1(input: String): Int {
         return input
             .split("\n\n")
-            .sumOf(::uniqueChars)
+            .sumOf(::allAnswers)
     }
 
-    private fun uniqueChars(string: String) =
+    fun solvePart2(input: String): Int {
+        return input
+            .split("\n\n")
+            .sumOf(::commonAnswers)
+    }
+
+    private fun allAnswers(string: String) =
         string
             .replace("\n", "")
             .toCharArray()
             .distinct()
+            .size
+
+    private fun commonAnswers(answers: String) =
+        answers
+            .split("\n")
+            .map { singlePersonAnswers -> singlePersonAnswers.toSet() }
+            .reduce { acc, i -> acc intersect i }
             .size
 }
--- src/test/kotlin/Day06Test.kt
+++ src/test/kotlin/Day06Test.kt
@@ -29,6 +29,36 @@ class Day06Test {
     fun `part1 actual data`() {
         val input = Resources.read("06.txt")
 
-        assertEquals(11, Day06().solvePart1(input))
+        assertEquals(6625, Day06().solvePart1(input))
+    }
+
+    @Test
+    fun `part2 provided example`() {
+        val input = """
+            abc
+
+            a
+            b
+            c
+
+            ab
+            ac
+
+            a
+            a
+            a
+            a
+
+            b
+        """.trimIndent()
+
+        assertEquals(6, Day06().solvePart2(input))
+    }
+
+    @Test
+    fun `part2 actual data`() {
+        val input = Resources.read("06.txt")
+
+        assertEquals(3360, Day06().solvePart2(input))
     }
 }
